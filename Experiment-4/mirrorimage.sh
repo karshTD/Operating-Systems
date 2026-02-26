8ee1b012fbc66a2042b3122932e10ed80efd8145
Utkarsh <132635254+karshTD@users.noreply.github.com>
2026-02-26 16:04:33 +0530

    Create mirrorimage.sh

---
+++ Experiment-4/mirrorimage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+read -p "Enter value of n: " n
+
+for ((i=1; i<=n; i++))
+do
+    for ((j=1; j<=i; j++))
+    do
+        echo -n "*"
+    done
+    echo
+done
+
+for ((i=n-1; i>=1; i--))
+do
+    for ((j=1; j<=i; j++))
+    do
+        echo -n "*"
+    done
+    echo
+done
